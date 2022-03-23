d5ec648347e66b2f1ed28074e3ec59a67824c0bf
zith1994 <zitharaurus@gmail.com>
2022-03-23 23:42:30 +0100

    test na pageach

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,10 @@ const routes: Routes = [{
         path: 'home', 
         loadChildren: () => HomePageModule
       },
+  { 
+        path: 'about-us', 
+        loadChildren: () => AboutUsPageModule
+      }
 ];
 
 @NgModule({
