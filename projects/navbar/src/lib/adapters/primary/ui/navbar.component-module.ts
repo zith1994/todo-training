076fcc95644bec3a707eafff10d11b9ea55c76d6
zith1994 <zitharaurus@gmail.com>
2022-03-30 00:14:24 +0200

    navbar

---
+++ projects/navbar/src/lib/adapters/primary/ui/navbar.component-module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './navbar.component';
+import { RouterModule } from '@angular/router';
+@NgModule({ imports: [CommonModule,RouterModule],
+  	declarations: [NavbarComponent],
+  	providers: [],
+  	exports: [NavbarComponent] })
+export class NavbarComponentModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { NavbarComponentModule } from '@navbar';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NavbarComponentModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
